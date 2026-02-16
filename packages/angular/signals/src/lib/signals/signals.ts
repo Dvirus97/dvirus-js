1fcce86c51ddc38eb564afea1340b95efd0727a3
Dvir Cohen <klik5456@gmail.com>
2026-02-17 01:25:39 +0200

    angular

---
+++ packages/angular/signals/src/lib/signals/signals.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-signals',
+  imports: [],
+  templateUrl: './signals.html',
+  styleUrl: './signals.css',
+})
+export class Signals {}
