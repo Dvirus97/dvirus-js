9d1cc31c4a3bc82ae7a02cf07e219d9cfa5bb78b
Dvir Cohen <klik5456@gmail.com>
2026-02-17 01:28:17 +0200

    utils lib

--- packages/angular/signals/src/lib/signals/signals.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'lib-signals',
-  imports: [],
-  templateUrl: './signals.html',
-  styleUrl: './signals.css',
-})
-export class Signals {}
