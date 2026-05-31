3c84e53d0fe4e7aec2ef4b90ef35e47c68a79ac7
Dvir Cohen <klik5456@gmail.com>
2026-05-31 11:16:45 +0300

    fix: add getEmptyKeys util

--- packages/angular/signals/src/lib/utils/index.ts
+++ packages/angular/signals/src/lib/utils/index.ts
@@ -1,10 +1,10 @@
-export  * from './signals.utils';
-export  * from './object-type';
-export  * from './signal-notifier';
-export  * from './signal-debounce';
-export  * from './signal-set';
-export  * from './signal-map';
-export  * from './signal-object';
-export  * from './control-signal';
-export  * from './try-catch';
-export  * from './writable-signal'; // before linkedSignal. < v18
+export * from './signals.utils';
+export * from './object-type';
+export * from './signal-notifier';
+export * from './signal-debounce';
+export * from './signal-set';
+export * from './signal-map';
+export * from './signal-object';
+export * from './control-signal';
+export * from './try-catch';
+export * from './writable-signal'; // before linkedSignal. < v18
